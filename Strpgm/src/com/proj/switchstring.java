985cd8afda8c95017d1a35f9519c858073b8c5a4
nisha <sreenishakurup@gmail.com>
2019-05-22 12:02:33 +0530

    commit project1

---
+++ Strpgm/src/com/proj/switchstring.java
@@ -0,0 +1,37 @@
+package com.proj;
+
+public class switchstring {
+
+	public static void main(String[] args) {
+		printColorUsingSwitch("red");
+		printColorUsingIf("red");
+		// switch case string is case sensitive
+		printColorUsingSwitch("RED");
+		printColorUsingSwitch(null);
+	}
+
+	private static void printColorUsingIf(String color) {
+		if (color.equals("blue")) {
+			System.out.println("BLUE");
+		} else if (color.equals("red")) {
+			System.out.println("RED");
+		} else {
+			System.out.println("INVALID COLOR CODE");
+		}
+	}
+
+	private static void printColorUsingSwitch(String color) {
+		switch (color) {
+		case "blue":
+			System.out.println("BLUE");
+			break;
+		case "red":
+			System.out.println("RED");
+			break;
+		default:
+			System.out.println("INVALID COLOR CODE");
+		}
+	}
+
+	
+}
